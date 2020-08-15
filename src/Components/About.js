f4ebe37f9d044949ba8544b98ca31b735cb58993
Gaurav Ramesh Ghatge <55950099+gauravghatge@users.noreply.github.com>
2020-08-15 18:04:25 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,6 +4,7 @@ export const About=()=>{
     return(
         <Container>
             <h3>App is under development so do test it to help improvising it!!!</h3>
+            <p>Developed By Gaurav G.</p>
         </Container>
     )
 } 
